a01d8583477aea6c7afa19175af8bcab696e4026
correialeo <leandroo774@outlook.com>
2024-08-21 10:02:32 -0300

    Router DOM

---
+++ aula06/src/App.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom"
+import Menu from "./routes/Menu"
+import styles from './App.module.css'
+
+export default function App(){
+  return(
+    <div className={styles.container}>
+      <Menu/>
+      <Outlet/>
+    </div>
+  )
+}
---
+++ aula06/src/routes/Home/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../../App.module.css"
+
+export default function Home(){
+    return(
+        <main className={styles.conteudo}>
+            <h2>Home</h2>
+        </main>
+    )
+}
