6dc937eca4b9f86a46123c0b170e91eba6723b08
akemilol <valeriaconcedsantos@gmail.com>
2024-08-21 09:51:38 -0300

    implementação do css

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Home from './routes/Home/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Error from './routes/Error/index.tsx';
+import Produtos from './routes/Produtos/index.tsx';
+
+const routes = createBrowserRouter([
+  {path: "/", element: <App />, errorElement: <Error />, children: [
+    {path: "/", element: <Home />},
+    {path: "/", element: <Produtos />},
+    {path: "/produtos/editar;:id", element: <EditarProdutos />},
+  ]
+
+
+  }]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={routes} />
+  <App />
   </StrictMode>,
 )
--- aula06/src/routes/Menu/index.tsx
+++ aula06/src/routes/Menu/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+import styles from "../../App.module.css";
 function Menu() {
+
     return(
-        <nav>
-            <li>Home</li>
-            <li>Produtos</li>
-            <li>Editar Produtos</li>
+        <nav className= {styles.menu}>
+            <li><Link to="/"> Home</Link></li>
+            <li><Link to="/produtos">Produtos</Link></li>
+            <li><Link to="/produtos/editar/0">Editar Produtos</Link></li>
         </nav> 
     );
 }
